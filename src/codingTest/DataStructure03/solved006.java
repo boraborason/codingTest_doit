74dd70b7dc01be358d4bcbd6d058438a69682787
Bora <alclsloo@naver.com>
2023-01-09 16:48:14 +0900

    연속된 자연수의 합구하기

--- src/codingTest/DataStructure03/solved006.java
+++ src/codingTest/DataStructure03/solved006.java
@@ -4,6 +4,25 @@ public class solved006 {
 
   public static void main(String[] args) throws Exception {
 
-  }
+    int N = 15; // 합 포인터가 15 하나를 가르킬 경우를 미리 초기화
+    int count = 1; //
+    int start_point = 1;
+    int end_point = 1;
+    int sum = 1;
 
+    while (end_point != N) { // end_point가 15일 때까지 루프
+      if (sum == N) { // 연속된 합이 N과 같을 경우
+        count++;
+        end_point++;
+        sum = sum + end_point;
+      } else if (sum > N) { // 연속된 합이 N과 같을 경우
+        sum = sum - start_point;
+        start_point++;
+      } else { // 연속된 합이 N과 같을 경우, 이동이동 1+2=3
+        end_point++;
+        sum = sum + end_point;
+      }
+    }
+    System.out.println("갯수 : " + count);
+  }
 }
